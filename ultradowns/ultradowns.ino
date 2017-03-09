a5da3f070c2aacac4ee92e9593cd9f29c4e46a7d
Anders Dahlen <anders.dahlen@knowit.no>
2017-03-09 21:17:58 +0100

    Cleaned up a little and added more sensors

--- ultradowns/ultradowns.ino
+++ ultradowns/ultradowns.ino
@@ -1,41 +1,84 @@
 #include <NewPing.h>
 
+// Ultrasound defines
 #define TRIGGER_PIN_1 11
 #define ECHO_PIN_1 12
-#define MAX_DISTANCE 200
-
 #define TRIGGER_PIN_2 10
 #define ECHO_PIN_2 13
+#define MAX_DISTANCE 200
 
+// Gas sensor defines
+#define GAS_SENSOR_PIN A0
+
+// Ultrasound setup 
 NewPing sonar1(TRIGGER_PIN_1, ECHO_PIN_1, MAX_DISTANCE);
 NewPing sonar2(TRIGGER_PIN_2, ECHO_PIN_2, MAX_DISTANCE);
 
 const unsigned int oversamples = 1;
-const unsigned int filterSize = 10;
-float filterValue1, filterValue2;
+const unsigned int filterSizeU = 10;
+float filterValueU1, filterValueU2;
 
-float processSensorValue(float &filteredValue, unsigned int newValue, unsigned int _filterSize) {
+// Gas sensor setup
+const unsigned int filterSizeGas = 10;
+float filterValueGas;
+
+
+
+// Sensor tags
+const String u1 = "u1:";      // Ultrasound sensor 1
+const String u2 = "u2:";      // Ultrasound sensor 2
+const String gas = "gas:";    // Gas sensor
+
+void setup() {
+   Serial.begin(9600);
+   filterValueU1 = sonar1.ping_cm();
+   filterValueU2 = sonar2.ping_cm();
+   filterValueGas = analogRead(GAS_SENSOR_PIN);
+
+   
+}
+ 
+void loop() {
+   // Sampling and processing ultrasound sensor
+   sampleUltrasound(filterValueU1, filterSizeU, oversamples);
+   sampleUltrasound(filterValueU2, filterSizeU, oversamples);
+
+   // Sampling and processing gas sensor
+   //gasRead = analogRead(GAS_SENSOR_PIN);
+   //processSensorValue(filterValueGas, gasRead, filterSizeGas);
+   sampleGasSensor(filterValueGas, filterSizeGas);
+
+   // Print values
+   printValue(u1, filterValueU1);
+   printValue(u2, filterValueU2);
+   printValue(gas, filterValueGas);
+}
+
+// sensor - sensor tag ending with ":". Eg. "u1:"
+void printValue(String sensor, float value) {
+  Serial.print(sensor);
+  Serial.print(value);
+  Serial.print('\n');
+}
+
+// Simlpe IIR filter
+void processSensorValue(float &filteredValue, unsigned int newValue, unsigned int _filterSize) {
   filteredValue = (((float)(_filterSize-1))/_filterSize) * filteredValue + (1.0f / _filterSize) * newValue;
   return filteredValue;
 }
 
-void setup() {
-   Serial.begin(9600);
-   filterValue1 = sonar1.ping_cm();
-   filterValue2 = sonar2.ping_cm();
-}
- 
-void loop() {
+// Function sampling and filtering ultrasound sensor
+void sampleUltrasound(float &filterValue, unsigned int filterSize, unsigned int oversamples) {
    for (int i = 0; i < oversamples; i++) {
-     unsigned int uS1 = sonar1.ping_cm();
-     unsigned int uS2 = sonar2.ping_cm();
-     processSensorValue(filterValue1, uS1, filterSize);
-     processSensorValue(filterValue2, uS2, filterSize);
+     unsigned int uS = sonar1.ping_cm();
+     processSensorValue(filterValue, uS, filterSize);
    }
-   Serial.print("u1:");
-   Serial.print(filterValue1);
-   Serial.print('\n');
-   Serial.print("u2:");
-   Serial.print(filterValue2);
-   Serial.print('\n');  
 }
+
+// Sample and filter gas sensor
+void sampleGasSensor(float &filterValue, unsigned int filterSize) {
+  asm(
+  unsigned int gasRead = analogRead(GAS_SENSOR_PIN);
+  processSensorValue(filterValue, gasRead, filterSizeGas);
+}
+
