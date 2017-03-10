01f2c2109cc2e22b2b7f95a998e9a99ed2e08ef3
Anders Dahlen <anders.dahlen@knowit.no>
2017-03-10 17:00:46 +0100

    Fixed interupts for all relevant sensors

--- ultradowns/ultradowns.ino
+++ ultradowns/ultradowns.ino
@@ -5,6 +5,7 @@
 #include "I2Cdev.h"
 #include "RTIMUSettings.h"
 #include "RTIMU.h"
+#include "RTMath.h"
 #include "RTFusionRTQF.h"
 #include "CalLib.h"
 #include <EEPROM.h>
@@ -23,6 +24,9 @@
 // GPS defines
 #define gpsTX 3
 #define gpsRX 4
+
+// Shadow clock timer
+unsigned int tick = 0;
 
 // Ultrasound setup
 NewPing sonar1(TRIGGER_PIN_1, ECHO_PIN_1, MAX_DISTANCE);
@@ -51,6 +55,9 @@ RTIMUSettings settings;
 const String u1 = "u1:";      // Ultrasound sensor 1
 const String u2 = "u2:";      // Ultrasound sensor 2
 const String gas = "gas:";    // Gas sensor
+const char* accl = "accl:";  // Accelerometer
+const char* gyro = "gyro:";  // Gyroscope
+const char* mag = "mag:";    // Magnetometer
 
 extern "C" {
   void setupTimer(void);
@@ -138,12 +145,32 @@ void sampleIMU(RTIMU *imu) {
 
 }
 
-
 // Called whenever TCNT1 overflows
 ISR(TIMER1_OVF_vect) {
-  TCNT1 = 34286; // 2 hz by filling the counter
-  Serial.println("interrupt");
-  sampleGasSensor(filterValueGas, filterSizeGas);
-  printValue(gas, filterValueGas);
+  TCNT1 = 34286; // 64 hz by filling the counter
+  
+  tick < 65536 ? tick++ : tick = 0; // Shadow tick counter
+
+  if(tick % 2 == 0) {
+    sampleIMU(imu);
+    RTMath::display(accl, (RTVector3&)imu->getAccel());
+    Serial.print('\n');
+    RTMath::display(gyro, (RTVector3&)imu->getGyro());
+    Serial.print('\n');
+    RTMath::display(mag, (RTVector3&)imu->getCompass());
+    Serial.print('\n');
+  }
+  if(tick % 8 == 0) { 
+    sampleUltrasound(sonar1.ping_cm(), filterValueU1, filterSizeU, oversamples);
+    printValue(u1, filterValueU1);
+    
+    sampleUltrasound(sonar2.ping_cm(), filterValueU2, filterSizeU, oversamples);
+    printValue(u2, filterValueU2);
+  }
+
+  if(tick % 64 == 0) {
+    sampleGasSensor(filterValueGas, filterSizeGas); 
+    printValue(gas, filterValueGas); 
+  }
 }
 
