0fcbc5dc908e74218650c667ab0c8cfb4fb8cb0b
Anders Dahlen <anders.dahlen@knowit.no>
2017-03-10 10:21:05 +0100

    added more sensors

--- ultradowns/ultradowns.ino
+++ ultradowns/ultradowns.ino
@@ -1,4 +1,6 @@
 #include <NewPing.h>
+#include <Adafruit_GPS.h>
+#include <SoftwareSerial.h>
 
 // Ultrasound defines
 #define TRIGGER_PIN_1 11
@@ -9,6 +11,10 @@
 
 // Gas sensor defines
 #define GAS_SENSOR_PIN A0
+
+// GPS defines
+#define gpsTX 3
+#define gpsRX 4
 
 // Ultrasound setup 
 NewPing sonar1(TRIGGER_PIN_1, ECHO_PIN_1, MAX_DISTANCE);
@@ -21,8 +27,11 @@ float filterValueU1, filterValueU2;
 // Gas sensor setup
 const unsigned int filterSizeGas = 10;
 float filterValueGas;
+boolean toggleGas=0;
 
-
+// GPS
+//SoftwareSerial gpsSerial(
+//Adafruit_GPS GPS
 
 // Sensor tags
 const String u1 = "u1:";      // Ultrasound sensor 1
@@ -30,28 +39,29 @@ const String u2 = "u2:";      // Ultrasound sensor 2
 const String gas = "gas:";    // Gas sensor
 
 void setup() {
-   Serial.begin(9600);
-   filterValueU1 = sonar1.ping_cm();
-   filterValueU2 = sonar2.ping_cm();
-   filterValueGas = analogRead(GAS_SENSOR_PIN);
-
-   
+  Serial.begin(9600);
+  filterValueU1 = sonar1.ping_cm();
+  filterValueU2 = sonar2.ping_cm();
+  //filterValueGas = analogRead(GAS_SENSOR_PIN);
+  //initInteruptTimer(1);
 }
  
 void loop() {
    // Sampling and processing ultrasound sensor
-   sampleUltrasound(filterValueU1, filterSizeU, oversamples);
-   sampleUltrasound(filterValueU2, filterSizeU, oversamples);
+   unsigned int uS1 = sonar1.ping_cm();
+   sampleUltrasound(uS1, filterValueU1, filterSizeU, oversamples);
+   unsigned int uS2 = sonar2.ping_cm();
+   sampleUltrasound(uS2, filterValueU2, filterSizeU, oversamples);
 
    // Sampling and processing gas sensor
    //gasRead = analogRead(GAS_SENSOR_PIN);
    //processSensorValue(filterValueGas, gasRead, filterSizeGas);
-   sampleGasSensor(filterValueGas, filterSizeGas);
+   //sampleGasSensor(filterValueGas, filterSizeGas);
 
    // Print values
    printValue(u1, filterValueU1);
    printValue(u2, filterValueU2);
-   printValue(gas, filterValueGas);
+
 }
 
 // sensor - sensor tag ending with ":". Eg. "u1:"
@@ -68,17 +78,52 @@ void processSensorValue(float &filteredValue, unsigned int newValue, unsigned in
 }
 
 // Function sampling and filtering ultrasound sensor
-void sampleUltrasound(float &filterValue, unsigned int filterSize, unsigned int oversamples) {
-   for (int i = 0; i < oversamples; i++) {
-     unsigned int uS = sonar1.ping_cm();
-     processSensorValue(filterValue, uS, filterSize);
+void sampleUltrasound(unsigned int sample, float &filterValue, unsigned int filterSize, unsigned int oversamples) {
+  for (int i = 0; i < oversamples; i++) {
+     processSensorValue(filterValue, sample, filterSize);
    }
 }
 
 // Sample and filter gas sensor
 void sampleGasSensor(float &filterValue, unsigned int filterSize) {
-  asm(
   unsigned int gasRead = analogRead(GAS_SENSOR_PIN);
   processSensorValue(filterValue, gasRead, filterSizeGas);
 }
+
+/*void initInteruptTimer(int hertz) {
+  cli();
+  asm(
+    "out %[tccr0a], 0x00\n"
+    "out %[tccr0b], 0x00\n"
+    "out %[tcnt0],  0x00\n"
+    "out %[ocr0a], 15624\n"
+    "ldi r14, 0x01\n"
+    "lsl r14, 0x00\n"
+    "ldi r15, 0x01\n"
+    "lsl r15, 0x02\n"
+    
+    "ldi r16, (1<<CS12)|(1<<CS10)\n"
+    "out %[tccr0b], r16\n"
+    "ldi r16, (1<<WGM01)\n"
+    "out %[tccr0a], r16\n"
+    "ldi r16,1<<OCIE0A\n"
+    "sts %[timsk0],r16\n"
+    "ret\n"
+    :: 
+    [tccr0a] "I" (_SFR_IO_ADDR(TCCR0A)),
+    [tccr0b] "I" (_SFR_IO_ADDR(TCCR0B)),
+    [tcnt0]  "I" (_SFR_IO_ADDR(TCNT0)),
+    [ocr0a]  "I" (_SFR_IO_ADDR(OCR0A)),
+    [timsk0] "I" (_SFR_IO_ADDR(TIMSK0))
+    );
+  sei();
+}
+
+ISR(TIMER0_COMPA_vect) {
+  if (toggleGas) {
+    sampleGasSensor(filterValueGas, filterSizeGas);
+    printValue(gas, filterValueGas);
+    toggleGas = 0;
+  }
+}*/
 
